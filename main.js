4d1a2f642b05791b315b4663ab7e23bc103ca3a0
Augusto Ana <augustora95@gmail.com>
2024-10-18 13:59:27 -0300

    Video 6

--- main.js
+++ main.js
@@ -2,3 +2,58 @@ import { renderCategories } from "./src/services/categories";
 import './style.css'
 
 renderCategories();
+
+
+
+/*Producto */
+
+const buttonAdd = document.getElementById("buttonAddElement");
+
+buttonAdd.addEventListener("click", () => {
+    openModal();
+})
+
+/*POPUP*/
+
+const cancelButton = document.getElementById("cancelButton")
+
+cancelButton.addEventListener('click', () =>{
+    handleCancelButton()
+})
+
+const handleCancelButton = () =>{
+    closeModal();
+}
+
+//Abrir y cerrar
+const openModal = () => {
+    const modal = document.getElementById("modalPopUP");
+    modal.style.display= "flex"
+}
+
+const closeModal = () => {
+    const modal = document.getElementById("modalPopUP");
+    modal.style.display= "none"
+}
+
+//Guardar o modificar elementos
+
+const acceptButton = document.getElementById("acceptButton");
+acceptButton.addEventListener("click", () =>{
+    handleSaveOrModifyElements();
+})
+
+const handleSaveOrModifyElements = () =>{
+    const nombre = document.getElementById("nombre").value,
+    imagen = document.getElementById("img").value,
+    precio = document.getElementById("precio").value,
+    categories = document.getElementById("categoria").value;
+    console.log({
+        nombre,
+        imagen,
+        precio
+    })
+
+    closeModal();
+
+}
